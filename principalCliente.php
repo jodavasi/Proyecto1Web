30575a82d01b4f53ec5c606ba444cfa372814c40
unknown <jodavasi@hotmail.com>
2020-07-20 10:09:11 -0600

    Areglos al los headers

--- principalCliente.php
+++ principalCliente.php
@@ -2,6 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
@@ -13,8 +14,8 @@
 
 
 
-    <h3>Cliente</h3>
-    
+<iframe frameborder = "0" src="headerCliente.php" class="navbar" height="100%" width="100%"></iframe>
+
     
 
 
